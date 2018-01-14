139bb099b92eb6a6b2ec22f3a8b0a8e4b3667e9d
Nguyễn Quang Minh <nguyenquangminh0711@gmail.com>
2018-01-14 14:31:00 +0700

    Setup dummy C extension

--- ext/yarv_generator/yarv_generator.c
+++ ext/yarv_generator/yarv_generator.c
@@ -1,4 +1,12 @@
 #include "ruby/ruby.h"
+#include "ruby/st.h"
+#include "ruby/encoding.h"
+#include "internal.h"
+#include "node.h"
+#include "parse.h"
+#include "symbol.h"
+#include "regenc.h"
+#include "probes.h"
 
 void Init_yarv_generator() {
 }
