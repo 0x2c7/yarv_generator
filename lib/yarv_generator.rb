ad7c67c6e13aea801039ccc097c8e3e43262862f
Nguyễn Quang Minh <nguyenquangminh0711@gmail.com>
2018-01-21 23:51:05 +0700

    Pretty print the iseq tree

--- lib/yarv_generator.rb
+++ lib/yarv_generator.rb
@@ -1,7 +1,14 @@
+require 'erb'
 require 'yarv_generator/version'
 class YarvGenerator
   def self.build_from_source(src)
     YarvGenerator::Builder.new.build_from_source(src)
+  end
+
+  def self.indent(str, span)
+    str.split("\n").map do |line|
+      "#{'  ' * span}#{line}"
+    end.join("\n")
   end
 end
 require 'yarv_generator/iseq'
--- lib/yarv_generator/instruction.rb
+++ lib/yarv_generator/instruction.rb
@@ -7,13 +7,40 @@ class YarvGenerator
 
   class Instruction
     attr_accessor :name, :operands, :label, :line_no
+
     def initialize(name, operands)
       @name = name
       @operands = operands
+    end
+
+    INSPECT_TEMPLATE = <<-INSPECT.strip
+<%- unless label.nil? -%>
+<Label <%= label -%>>
+<%- end -%>
+<%- if operands.any? { |o| o.is_a?(YarvGenerator::Iseq) } -%>
+<%= name %>
+  <%- operands.each_with_index do |o, index|-%>
+    <%- if o.is_a?(YarvGenerator::Iseq) -%>
+<%= YarvGenerator.indent(o.inspect, 1) %>
+    <%- else -%>
+<%= YarvGenerator.indent(o.respond_to?(:inspect) ? o.inspect : o, 1) %>
+    <%- end -%>
+  <%- end -%>
+<%- else -%>
+<%= name%> <%= operands.map { |o| o.respond_to?(:inspect) ? o.inspect : o }.join(", ") %>
+<%- end -%>
+    INSPECT
+
+    def inspect
+      ERB.new(INSPECT_TEMPLATE, nil, '-').result(binding)
     end
   end
 
   class CallInfo
     attr_accessor :mid, :flag, :orig_argc, :kw_arg
+
+    def inspect
+      "method(#{mid}, orig_argc: #{orig_argc})"
+    end
   end
 end
